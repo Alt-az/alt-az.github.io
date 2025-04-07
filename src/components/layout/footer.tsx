8b8d74336c8b8e2318a1fd87c34b4018d957076b
Piotr Spętany <kapj24l-4@sr.dmcs.pl>
2025-04-07 21:27:52 +0200

    some changes v5

--- src/components/layout/footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-secondary text-white py-4 px-6 mt-auto">
-      <div className="container mx-auto text-center text-sm">
-        <p>© {new Date().getFullYear()} PGF Assistant. All rights reserved.</p>
-      </div>
-    </footer>
-  );
-}
